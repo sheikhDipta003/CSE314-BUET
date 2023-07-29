d311591b3a493852c283ea51d6c21f7cc03ed21f
sheikhdipta <sheikhdipta@gmail.com>
2023-07-29 21:59:14 +0600

    IPC : tried to integrate the logical construct of Dining-Philosopher Problem

--- Inter-Process-Communication/mycodes/asgn_solve.cpp
+++ Inter-Process-Communication/mycodes/asgn_solve.cpp
@@ -7,8 +7,12 @@
 #include <chrono>
 #include <random>
 
-#define N_PS 4
+#define N_PS 3
 #define N_BS 2
+#define INACTIVE 0
+#define WAITING 1
+#define USING 2
+
 #define timestamp std::chrono::high_resolution_clock::time_point
 #define current_time() std::chrono::high_resolution_clock::now()
 #define get_seconds(t2, t1) duration_cast<seconds>(t2 - t1).count()
@@ -18,7 +22,7 @@ using namespace std::chrono;
 
 std::random_device rd;          // obtain a random seed from the hardware
 std::mt19937 generator(rd());   // Mersenne Twister 19937 engine
-double mean_delay = 1.0;        // adjust to control expected delay
+double mean_delay = 1.6;        // adjust to control expected delay
 poisson_distribution<int> poisson(mean_delay);
 
 timestamp initialTime;
@@ -26,7 +30,8 @@ int N, M;   // N -> number of students, M -> size of each group
 int w, x, y;    // relative time units for the operations printing, binding and reading/writing respectively
 bool* psIsFree;
 pthread_mutex_t mutex_ps;
-sem_t sem_availablePS;
+sem_t* sem_ps;
+int* state;
 
 void printOut(int id, string description, int time) {
     string msg;
@@ -34,40 +39,66 @@ void printOut(int id, string description, int time) {
     cout << msg;
 }
 
-void usePS(int id, int time, timestamp lastTime) {
-    sem_wait(&sem_availablePS);
-    
+void test(int id, int pid){
+    if(state[id]==WAITING && psIsFree[pid]){
+        state[id] = USING;
+        psIsFree[pid] = false;
+        sem_post(&sem_ps[id]);
+    }
+}
+
+void take_PS(int id){
     pthread_mutex_lock(&mutex_ps);
-    int i = (id % 4) + 1;         // student no. 'id' is assigned to the print station no. (id % 4) + 1
-    if(psIsFree[i]) psIsFree[i] = false;
+
+    state[id] = WAITING;
+    int pid = (id % N_PS) + 1;         // student no. 'id' is assigned to the print station no. (id % N_PS) + 1
+    test(id, pid);
+
     pthread_mutex_unlock(&mutex_ps);
+    sem_wait(&sem_ps[id]);
+}
+
+void put_PS(int id){
+    pthread_mutex_lock(&mutex_ps);
+
+    state[id] = INACTIVE;
+    psIsFree[id] = true;
+
+    //notify all teammates and then rest of the students who are waiting for this PS
+    int g = (id-1) / M + 1;      // 1 <= g <= (N/M)
+    int s = 1 + (g-1) * M;       // starting student-id of group no. 'g'
+    int pid = (id % N_PS) + 1;
+    for(int i = s; i < s+M; i++){
+        if(i != id) test(i, pid);
+    }
+    for(int i = 1; i <= N; i++){
+        if(i < s || i >= s+M) test(i, pid);
+    }
+
+    pthread_mutex_unlock(&mutex_ps);
+}
+
+void usePS(int id, int time, timestamp lastTime) {    
+    take_PS(id);
 
     time += get_seconds(current_time(), lastTime);
-
+    int i = (id % N_PS) + 1;
     printOut(id, "has started printing at print station " + to_string(i), time);
     sleep(w);
     time += w;
     printOut(id, "has finished printing at print station " + to_string(i), time);
 
-    pthread_mutex_lock(&mutex_ps);
-    psIsFree[i] = true;
-    pthread_mutex_unlock(&mutex_ps);
-
-    sem_post(&sem_availablePS);
-
-    // checkSecurity(id, isVip, time, current_time());
+    put_PS(id);
 }
 
 void* arrivePS(void* arg) {
     int id = *((int*)arg);
-    delete (int*)arg;
-    arg = nullptr;
 
     timestamp t = current_time();
     int time = get_seconds(t, initialTime);
-    int pid = (id % 4) + 1;     //The rule to assign a student to a printer is (student ID mod 4 + 1)
     printOut(id, "has arrived at the print station", time);
 
+    state[id] = WAITING;
     usePS(id, time, current_time());
 
     return NULL;
@@ -84,7 +115,16 @@ int main() {
     fill(psIsFree, psIsFree + N_PS, true);
 
     pthread_mutex_init(&mutex_ps, NULL);
-    sem_init(&sem_availablePS, 0, N_PS);
+    state = new int[N];
+    fill(state, state + N, INACTIVE);
+    sem_ps = new sem_t[N];
+    for (int i = 0; i < N; i++) {       // Initialize each semaphore in the array
+        if (sem_init(&sem_ps[i], 0, 1) != 0) {
+            cout << "Semaphore initialization failed for index " << i << std::endl;
+            free(sem_ps);
+            return 1;
+        }
+    }
 
     int ret;                        //  To read the return value from each `pthread` methods
     int sid = 0;                    //  Total count of students for uniquely identifying
@@ -111,6 +151,13 @@ int main() {
             exit(-1);
         }
     }
+
+    for (int i = 0; i < N; ++i) {
+        sem_destroy(&sem_ps[i]);
+    }
+
+    free(sem_ps);
+    free(state);
 
     return 0;
 }
